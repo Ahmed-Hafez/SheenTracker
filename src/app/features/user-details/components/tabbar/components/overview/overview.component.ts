2b0641d431a4447236c53dce0c60391de49e9f39
Ahmed Mohammed Hany <ahmedhany20200050@gmail.com>
2026-06-11 16:56:31 +0300

    navigation to squads from user details done

--- src/app/features/user-details/components/tabbar/components/overview/overview.component.ts
+++ src/app/features/user-details/components/tabbar/components/overview/overview.component.ts
@@ -2,7 +2,7 @@ import { Component, ChangeDetectionStrategy, input } from '@angular/core';
 import { UserSummaryComponent } from '../../../user-summary/user-summary.component';
 import { SystemUser } from '../../../../../../core/models/reponse/system-users.response.model';
 import { Router } from '@angular/router';
-import{ inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { Departments } from '../../../../../../core/enums/departments.enum';
 import { Seniorities } from '../../../../../../core/enums/seniority.enum';
 import { EnumLabelPipe } from '../../../../../../core/pipes/enum-label-pipe';
@@ -20,20 +20,37 @@ export class OverviewComponent {
   departments = Departments;
   seniorities = Seniorities;
 
-   navigateToUser(userId: number|null|undefined): void {
+  navigateToUser(userId: number | null | undefined): void {
     console.log('Navigating to user details for userId:', userId);
-    if(userId === null || userId === undefined) {
-      console.warn('User ID is null or undefined in overview tab. Cannot navigate to user details.');
+    if (userId === null || userId === undefined) {
+      console.warn(
+        'User ID is null or undefined in overview tab. Cannot navigate to user details.',
+      );
       return;
     }
 
-
     const url = this.router.serializeUrl(
-  this.router.createUrlTree(['/users'], {
-    queryParams: { userId }
-  })
-);
+      this.router.createUrlTree(['/users'], {
+        queryParams: { userId },
+      }),
+    );
 
-window.open(url, '_blank');
+    window.open(url, '_blank');
+  }
+
+  navigateToSquad(squadId: number | null | undefined): void {
+    console.log('Navigating to squad details for squadId:', squadId);
+    if (squadId === null || squadId === undefined) {
+      console.warn(
+        'Squad ID is null or undefined in overview tab. Cannot navigate to squad details.',
+      );
+      return;
+    }
+    const url = this.router.serializeUrl(
+      // squads/1
+      this.router.createUrlTree(['squads', squadId]),
+    );
+
+    window.open(url, '_blank');
   }
 }
