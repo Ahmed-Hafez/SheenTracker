529714479b5354ad59cf4072419f25749632595a
Ahmed Mohammed Hany <ahmedhany20200050@gmail.com>
2026-08-18 12:03:46 +0300

    added  all epics route and made the routing button work

---
+++ src/app/features/quarter-plans-all-epics/quarter-plans-all-epics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quarter-plans-all-epics',
+  imports: [],
+  templateUrl: './quarter-plans-all-epics.component.html',
+  styleUrl: './quarter-plans-all-epics.component.scss',
+})
+export class QuarterPlansAllEpicsComponent {
+
+}
--- src/app/features/quarter-plans/components/epics-by-area/epics-by-area.component.ts
+++ src/app/features/quarter-plans/components/epics-by-area/epics-by-area.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AreaPathComponent } from './area-path/area-path.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-epics-by-area',
@@ -7,4 +8,11 @@ import { AreaPathComponent } from './area-path/area-path.component';
   templateUrl: './epics-by-area.component.html',
   styleUrl: './epics-by-area.component.scss',
 })
-export class EpicsByAreaComponent {}
+export class EpicsByAreaComponent {
+  constructor(private router: Router) {}
+
+  navigateToAllEpics() {
+    this.router.navigate(['/quarter-plans/all-epics']);
+  }
+
+}
